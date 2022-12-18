c66ba145497be9c0b074ecf16765537866fab223
NorbertAll <norbert_gutkowski@interia.pl>
2022-12-18 15:31:10 +0100

    create class Deck

--- app/Game.js
+++ app/Game.js
@@ -1,5 +1,5 @@
-import { Card } from './Card.js';
+import { Deck } from "./Deck.js";
 
-const card = new Card('A', 'hearts');
-document.getElementById('playersCards').appendChild(card.render())
 
+const deck= new Deck();
+console.log(deck.shuffle());
