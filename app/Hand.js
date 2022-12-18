116bd09ea46d9e5ffd9cca32cd2c27875f7e6b7f
NorbertAll <norbert_gutkowski@interia.pl>
2022-12-18 21:14:10 +0100

    player points calculation

--- app/Hand.js
+++ app/Hand.js
@@ -5,4 +5,33 @@ export class Hand{
     addCard(card){
         this.cards.push(card);
     }
+    countCardByWeight(weight){
+        return this.cards.filter(card=>card.weight==weight).length;
+    }
+    getStrngth(){
+        const cards= this.cards.map(card=>{
+            if('A'.includes(card.weight)){
+                return 11;
+            }
+            if('K'.includes(card.weight)){
+                return 4;
+            }
+            if('Q'.includes(card.weight)){
+                return 3;
+            }
+            if('J'.includes(card.weight)){
+                return 2;
+            }
+            if('9'.includes(card.weight)){
+                return 0;
+            }
+            if('10'.includes(card.weight)){
+                return 10;
+            }
+            return parseInt(card.weight);
+        });
+        return cards.reduce(function(sum, weight){
+            return parseInt(sum)+parseInt(weight);
+        })
+    }
 }
